d60e40d3f401e1e0b001e965a1d267d1ad94151d
SergioGallardo2697 <sergiogallardo2697@gmail.com>
2026-06-13 09:03:58 -0700

    Se modifica el menu

--- src/app/paginas/layout/layout.ts
+++ src/app/paginas/layout/layout.ts
@@ -1,11 +1,8 @@
-import { Component, inject, signal, HostListener } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Router, RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { MatTooltipModule } from '@angular/material/tooltip';
 import { AutenticacionService } from '../../core/servicios/autenticacion.service';
 
 @Component({
@@ -14,12 +11,9 @@ import { AutenticacionService } from '../../core/servicios/autenticacion.service
     RouterOutlet,
     RouterLink,
     RouterLinkActive,
-    MatSidenavModule,
-    MatListModule,
-    MatToolbarModule,
+    MatMenuModule,
     MatIconModule,
-    MatButtonModule,
-    MatTooltipModule
+    MatButtonModule
   ],
   templateUrl: './layout.html',
   styleUrl: './layout.scss'
@@ -33,25 +27,6 @@ export class Layout {
     { ruta: '/paypals', icono: 'account_balance', etiqueta: 'Paypals' },
     { ruta: '/compras', icono: 'shopping_cart', etiqueta: 'Compras' }
   ];
-
-  esMobile = signal(window.innerWidth < 768);
-  menuAbierto = signal(true);
-
-  @HostListener('window:resize')
-  onResize() {
-    this.esMobile.set(window.innerWidth < 768);
-  }
-
-  toggleMenu() {
-    this.menuAbierto.set(!this.menuAbierto());
-  }
-
-  navegarY(ruta: string) {
-    this.router.navigate([ruta]);
-    if (this.esMobile()) {
-      this.menuAbierto.set(false);
-    }
-  }
 
   obtenerIniciales(texto: string): string {
     const partes = texto.trim().split(/\s+/);
